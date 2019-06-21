f79e656cc4211af22e0234d5ec67c16d899e6d9b
Raphaël Pinson <raphael.pinson@camptocamp.com>
2019-06-21 11:42:24 +0200

    pass photoset_id as param

--- flickr_album.rb
+++ flickr_album.rb
@@ -5,6 +5,7 @@ require 'aws-sdk-rekognition'
 require 'open-uri'
 require 'json'
 
+photoset_id = ARGV[0]
 
 FlickRaw.api_key = ENV['FLICKR_API_KEY']
 FlickRaw.shared_secret = ENV['FLICKR_SHARED_SECRET']
@@ -14,13 +15,6 @@ rekognition = Aws::Rekognition::Client.new(
   region: 'eu-west-1',
   profile: 'perso',
 )
-#photoset_id = 72157670786830428
-#photoset_id = 72157602655176620
-#photoset_id = 72157638175663535
-#photoset_id = 72157628633710511
-#photoset_id = 72157630844398696
-#photoset_id = 72157673036030157
-photoset_id = 72157661354163663
 set = flickr.photosets.getPhotos(photoset_id: photoset_id)
 
 set.photo.each do |pic|
@@ -32,7 +26,7 @@ set.photo.each do |pic|
   puts "Importing #{ref} (#{url}) into collection"
 
   rekognition.index_faces({
-      collection_id: 'flick-72157670786830428',
+      collection_id: 'flickr',
       image: { bytes: img.read },
       external_image_id: ref,
   })
